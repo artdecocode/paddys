552ffbf1cf21278924db11061eed1e63d77a0949
Anton <anton@adc.sh>
2018-10-16 00:38:42 +0300

    package

--- build/index.js
+++ build/index.js
@@ -1,13 +1,34 @@
 /**
  * Pad Strings.
- * @param {string} [string] The string to pad.
  * @param {number} [width] The width to which to pad.
+ * @param {string} [string] The string to pad.
  */
-const paddys = (string, width) => {
+const pad = (width, string) => {
   const d = Math.max(width - string.length, 0)
   const p = ' '.repeat(d)
   const s = `${string}${p}`
   return s
+}
+
+/**
+ * Pads the string so that each line is of equal width.
+ * @param {string} [string] The string to pad.
+ * @param {Config} [options] Options for the program.
+ * @param {number} options.width The width to which to pad lines.
+ * @example
+ * import paddys from 'paddys'
+ * const res = paddys(`test
+t`)
+  // test
+  // t___
+ */
+const paddys = (string, options = {}) => {
+  const {
+    width,
+  } = options
+  const s = string.split('\n')
+  const w = width || s.reduce((acc, { length: l }) => l > acc ? l : acc, 0)
+  return s.map(pad.bind(null, w)).join('\n')
 }
 
 module.exports=paddys
@@ -15,6 +36,5 @@ module.exports=paddys
 /* documentary types/index.xml */
 /**
  * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
+ * @prop {number} width The width to which to pad lines.
  */
